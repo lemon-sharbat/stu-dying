b8446cd874bd71dff13c9cd9117ab07bda54e1f6
lemon-sharbat <147016246+lemon-sharbat@users.noreply.github.com>
2023-10-05 09:05:17 +0530

    Update and rename README.md to README.py
    hehee

---
+++ README.py
@@ -0,0 +1,17 @@
+# stu-dying
+#to study git hub
+
+#this repository exists just to take you around the world of python**
+
+#write a python program to check whethwer the entered character is lowercase or uppercase
+
+ch=input("enter character:")
+
+if(ch>='a' and ch<='z'):
+       print(ch,"is in lowerecase")
+
+elif(ch>='A' and ch<='Z'):
+       print(ch,"is in uppercase")
+
+else:
+       print("Charachter not applicable")
